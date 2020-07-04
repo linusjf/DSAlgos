b386ab5bdcd512b5a5a95dd7eea964823fce9e84
fernal73 <linus.fernandes@gmail.com>
2020-07-04 07:20:31 +0530

    Add repeatable flakeable trst.

--- src/test/java/ds/tests/HighArrayTest.java
+++ src/test/java/ds/tests/HighArrayTest.java
@@ -5,12 +5,16 @@ import static org.mockito.Mockito.*;
 
 import ds.HighArray;
 import java.util.ConcurrentModificationException;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import java.util.stream.LongStream;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
@@ -19,24 +23,21 @@ import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
 @TestInstance(Lifecycle.PER_CLASS)
-@Execution(ExecutionMode.SAME_THREAD)
+@Execution(ExecutionMode.CONCURRENT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SuppressWarnings("PMD.LawOfDemeter")
 class HighArrayTest {
   private static final Logger LOGGER = Logger.getLogger(HighArrayTest.class.getName());
-  HighArray arr;
   HighArray array;
 
   HighArrayTest() {
-    arr = new HighArray(100);
     array = new HighArray(10_000, true);
     LongStream nos = LongStream.rangeClosed(1L, 10_000L);
     nos.forEach(i -> array.insert(i));
   }
 
-  @Test
-  @Order(1)
-  void testInsert() {
+  HighArray insertElements() {
+    HighArray arr = new HighArray(100);
     // insert 10 items
     arr.insert(77L);
     arr.insert(99L);
@@ -48,12 +49,20 @@ class HighArrayTest {
     arr.insert(00L);
     arr.insert(66L);
     arr.insert(33L);
+    return arr;
+  }
+
+  @Test
+  @Order(1)
+  void testInsert() {
+    HighArray arr = insertElements();
     assertEquals(arr.count(), 10, "10 elements inserted.");
   }
 
   @Test
   @Order(2)
   void testDeleteTrue() {
+    HighArray arr = insertElements();
     LOGGER.info(() -> arr.toString());
     assertTrue(
         () -> arr.delete(00L) && arr.delete(55L) && arr.delete(99L),
@@ -63,15 +72,17 @@ class HighArrayTest {
   @Test
   @Order(3)
   void testDeleteFalse() {
+    HighArray arr = insertElements();
     LOGGER.info(() -> arr.toString());
     assertFalse(
-        () -> arr.delete(00L) || arr.delete(55L) || arr.delete(99L),
-        () -> "Elements 00, 55, 99 deleted");
+        () -> arr.delete(12L) || arr.delete(6L) || arr.delete(5L),
+        () -> "Elements 12, 6, 5 not deleted");
   }
 
   @Test
   @Order(4)
   void testFindIndexFalse() {
+    HighArray arr = insertElements();
     long searchKey = 35L;
     assertEquals(arr.findIndex(searchKey), -1, () -> searchKey + " not available");
   }
@@ -79,6 +90,7 @@ class HighArrayTest {
   @Test
   @Order(4)
   void testFindFalse() {
+    HighArray arr = insertElements();
     long searchKey = 35L;
     assertFalse(arr.find(searchKey), () -> searchKey + " not available");
   }
@@ -86,6 +98,7 @@ class HighArrayTest {
   @Test
   @Order(4)
   void testFindIndexTrue() {
+    HighArray arr = insertElements();
     long searchKey = 11L;
     assertTrue(arr.findIndex(searchKey) >= 0, () -> searchKey + " available");
   }
@@ -93,6 +106,7 @@ class HighArrayTest {
   @Test
   @Order(4)
   void testFindTrue() {
+    HighArray arr = insertElements();
     long searchKey = 11L;
     assertTrue(arr.find(searchKey), () -> searchKey + " available");
   }
@@ -100,6 +114,7 @@ class HighArrayTest {
   @Test
   @Order(5)
   void testClear() {
+    HighArray arr = insertElements();
     arr.clear();
     // for code coverage
     arr.clear();
@@ -109,6 +124,8 @@ class HighArrayTest {
   @Test
   @Order(6)
   void testToString() {
+    HighArray arr = insertElements();
+    arr.clear();
     arr.insert(77L);
     arr.insert(99L);
     arr.insert(44L);
@@ -120,6 +137,7 @@ class HighArrayTest {
   @Test
   @Order(7)
   void testDisplay() {
+    HighArray arr = insertElements();
     HighArray highArray = spy(arr);
 
     doAnswer(
@@ -137,90 +155,109 @@ class HighArrayTest {
 
   @Test
   void testException() {
-    HighArray array = new HighArray(3);
-    array.insert(2L);
-    array.insert(11L);
-    array.insert(21L);
+    HighArray highArray = new HighArray(3);
+    highArray.insert(2L);
+    highArray.insert(11L);
+    highArray.insert(21L);
     assertThrows(
         ArrayIndexOutOfBoundsException.class,
         () -> {
-          array.insert(45L);
+          highArray.insert(45L);
         });
   }
 
   @Test
   void testConcurrentInserts() {
-    HighArray array = new HighArray(10_000);
-    LongStream.rangeClosed(1L, 10_000L).parallel().forEach(i -> array.insert(i));
-    assertEquals(10_000, array.count(), () -> "10,000 elements filled: " + array.toString());
+    HighArray highArray = new HighArray(10_000);
+    LongStream.rangeClosed(1L, 10_000L).parallel().forEach(i -> highArray.insert(i));
+    assertEquals(
+        10_000, highArray.count(), () -> "10,000 elements filled: " + highArray.toString());
   }
 
   @Test
   void testConcurrentDeletes() {
-    HighArray array = new HighArray(10_000, true);
+    Random random = new Random();
+    HighArray highArray = new HighArray(10_000, true);
     LongStream nos = LongStream.rangeClosed(1L, 10_000L);
-    nos.forEach(i -> array.insert(i));
+    nos.forEach(i -> highArray.insert(i));
+
     LongStream nosParallel = LongStream.rangeClosed(1L, 10_000L).parallel();
     assertThrows(
-        ConcurrentModificationException.class, () -> nosParallel.forEach(i -> array.delete(i)));
+        ConcurrentModificationException.class,
+        () ->
+            nosParallel.forEach(
+                i -> {
+                  highArray.delete(i);
+                  try {
+                    TimeUnit.MILLISECONDS.sleep(random.nextInt(100));
+                  } catch (InterruptedException exc) {
+                    Thread.currentThread().interrupt();
+                  }
+                }));
   }
 
   @Test
   void testSequentialDeletes() {
-    HighArray array = new HighArray(10_000, true);
+    HighArray highArray = new HighArray(10_000, true);
     LongStream nos = LongStream.rangeClosed(1L, 10_000L);
     nos.forEach(
         i -> {
-          array.insert(i);
-          array.delete(i);
+          highArray.insert(i);
+          highArray.delete(i);
         });
-    assertEquals(0, array.count(), () -> "10,000 elements deleted: " + array.toString());
+    assertEquals(0, highArray.count(), () -> "10,000 elements deleted: " + highArray.toString());
   }
 
   @Test
   void testConcurrentSyncDeletes() {
-    HighArray array = new HighArray(100);
+    HighArray highArray = new HighArray(100);
     LongStream nos = LongStream.rangeClosed(1L, 10_000L);
     nos.forEach(
         i -> {
-          array.insert(i);
-          array.syncDelete(i);
+          highArray.insert(i);
+          highArray.syncDelete(i);
         });
-    assertEquals(0, array.count(), () -> "100 elements deleted: " + array.toString());
+    assertEquals(0, highArray.count(), () -> "100 elements deleted: " + highArray.toString());
   }
 
   @Test
   void testConcurrentInsertsDeletes() {
-    HighArray array = new HighArray(1_000_000, true);
+    Random random = new Random();
+    HighArray highArray = new HighArray(1_000_000, true);
     assertThrows(
         ConcurrentModificationException.class,
         () -> {
-          LongStream nos = LongStream.rangeClosed(1L, 1_000_000L).parallel();
+          LongStream nos = LongStream.rangeClosed(1L, 10000L).parallel();
           nos.forEach(
               i -> {
-                array.insert(i);
-                array.delete(i);
+                highArray.insert(i);
+                try {
+                  TimeUnit.MILLISECONDS.sleep(random.nextInt(100));
+                } catch (InterruptedException exc) {
+                  Thread.currentThread().interrupt();
+                }
+                highArray.delete(i);
               });
         });
   }
 
   @Test
   void testConcurrentSyncInsertsDeletes() {
-    HighArray array = new HighArray(100);
+    HighArray highArray = new HighArray(100);
     LongStream nos = LongStream.rangeClosed(1L, 100L).parallel();
     nos.forEach(
         i -> {
-          array.insert(i);
-          array.syncDelete(i);
+          highArray.insert(i);
+          highArray.syncDelete(i);
         });
-    assertEquals(0, array.count(), () -> "Elements cleared");
+    assertEquals(0, highArray.count(), () -> "Elements cleared");
   }
 
-  /**
-   * @RepeatedTest(10_000) void repeatedTestWithRepetitionInfo(RepetitionInfo repetitionInfo) {
-   * array.delete(repetitionInfo.getCurrentRepetition()); assertEquals(10_000,
-   * repetitionInfo.getTotalRepetitions()); }
-   */
+  @RepeatedTest(10_000)
+  void repeatedTestWithRepetitionInfo(RepetitionInfo repetitionInfo) {
+    array.delete(repetitionInfo.getCurrentRepetition());
+    assertEquals(10_000, repetitionInfo.getTotalRepetitions());
+  }
 
   /** Added tests for code coverage completeness. */
   @Test
