5b3a83a96f78cd0d401b819428dc57627b2785e3
fernal73 <linus.fernandes@gmail.com>
2020-07-19 20:43:53 +0530

    Convert HighArray concurrentDeletes to CountdownLatch

--- src/test/java/ds/tests/HighArrayConcurrencyTest.java
+++ src/test/java/ds/tests/HighArrayConcurrencyTest.java
@@ -4,20 +4,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import ds.HighArray;
 import java.util.ConcurrentModificationException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @TestInstance(Lifecycle.PER_CLASS)
 @Execution(ExecutionMode.CONCURRENT)
 @SuppressWarnings("PMD.LawOfDemeter")
 class HighArrayConcurrencyTest {
   private static final Logger LOGGER = Logger.getLogger(HighArrayConcurrencyTest.class.getName());
+
+  private Stream<Integer> provideInsertArraySize() {
+    Runtime rt = Runtime.getRuntime();
+    int processors = rt.availableProcessors();
+    long memory = rt.totalMemory();
+    LOGGER.info(() -> String.format("Memory: %d Processors: %d %n", memory, processors));
+    if (processors <= 2) return Stream.of(20_000);
+    else return Stream.of(5000);
+  }
 
   @Test
   void testConcurrentInserts() {
@@ -27,24 +42,40 @@ class HighArrayConcurrencyTest {
         10_000, highArray.count(), () -> "10,000 elements not filled: " + highArray.toString());
   }
 
-  @Test
-  void testConcurrentDeletes() {
+  @ParameterizedTest
+  @MethodSource("provideInsertArraySize")
+  void testConcurrentDeletes(int size) {
+    ExecutorService service = Executors.newFixedThreadPool(10);
+    CountDownLatch cdl = new CountDownLatch(1);
+    CountDownLatch done = new CountDownLatch(size);
     AtomicInteger excCount = new AtomicInteger();
-    HighArray highArray = new HighArray(10_000, true);
-    LongStream nos = LongStream.rangeClosed(1L, 10_000L);
+    HighArray highArray = new HighArray(size, true);
+    LongStream nos = LongStream.rangeClosed(1L, (long) size).unordered();
     nos.forEach(i -> highArray.insert(i));
-    LongStream nosParallel = LongStream.rangeClosed(1L, 10_000L).parallel();
+    LongStream nosParallel = LongStream.rangeClosed(1L, (long) size).unordered().parallel();
     nosParallel.forEach(
         i ->
-            new Thread(
-                    () -> {
-                      try {
-                        highArray.delete(i);
-                      } catch (ConcurrentModificationException cme) {
-                        excCount.incrementAndGet();
-                      }
-                    })
-                .start());
+            service.execute(
+                () -> {
+                  try {
+                    cdl.await();
+                    highArray.delete(i);
+                    done.countDown();
+                  } catch (ConcurrentModificationException cme) {
+                    LOGGER.severe(() -> "Error deleting " + i);
+                    excCount.incrementAndGet();
+                    done.countDown();
+                  } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                    done.countDown();
+                  }
+                }));
+    try {
+      cdl.countDown();
+      done.await();
+    } catch (InterruptedException ie) {
+      Thread.currentThread().interrupt();
+    }
     assertNotEquals(0, excCount.get(), () -> excCount + " is number of concurrent exceptions.");
   }
 
