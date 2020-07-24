b9c30992b286d38d43eacdee61b96d86870095e5
fernal73 <linus.fernandes@gmail.com>
2020-07-24 10:55:46 +0530

    Add class to create OrdArray reflectively

--- src/test/java/ds/tests/OrdArrayArgumentsAggregator.java
+++ src/test/java/ds/tests/OrdArrayArgumentsAggregator.java
@@ -2,6 +2,7 @@ package ds.tests;
 
 import ds.IArray;
 import ds.OrdArray;
+import java.util.Optional;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
@@ -11,7 +12,11 @@ class OrdArrayArgumentsAggregator implements ArgumentsAggregator {
 
   public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context)
       throws ArgumentsAggregationException {
-    IArray array = new OrdArray();
+    Integer size = Optional.ofNullable(accessor.getInteger(0)).orElse(100);
+    Boolean strict = Optional.ofNullable(accessor.getBoolean(1)).orElse(false);
+    Object[] args = accessor.toArray();
+    IArray array = new OrdArray(size, strict);
+    for (int i = 2; i < args.length; i++) array.insert((long) args[i]);
     return array;
   }
 }
