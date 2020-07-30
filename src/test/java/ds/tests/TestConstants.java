bd9993e1f51474e2e576a332d8fbbc8b5dde76c2
fernal73 <linus.fernandes@gmail.com>
2020-07-30 19:34:39 +0530

    Use Format annotation for Checkers in test classes only

--- src/test/java/ds/tests/TestConstants.java
+++ src/test/java/ds/tests/TestConstants.java
@@ -1,8 +1,11 @@
 package ds.tests;
 
+import org.checkerframework.checker.formatter.qual.ConversionCategory;
+import org.checkerframework.checker.formatter.qual.Format;
+
 public final class TestConstants {
 
-  static final String NOT_AVAILABLE = "%d not available";
+  static final @Format(ConversionCategory.INT) String NOT_AVAILABLE = "%d not available";
 
   static final String SORTED = "sorted";
 
