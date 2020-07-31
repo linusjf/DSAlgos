f6dd154a2c1bd9a2e43a51d59701a22f6b51858d
fernal73 <linus.fernandes@gmail.com>
2020-07-31 11:47:19 +0530

    Add leafnode tests for abstract array classes

---
+++ src/test/java/ds/tests/AbstractArrayTest.java
@@ -0,0 +1,43 @@
+package ds.tests;
+
+import static ds.tests.TestConstants.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import ds.AbstractArray;
+import ds.AbstractOrdArray;
+import ds.OrdArray;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@Execution(ExecutionMode.SAME_THREAD)
+@SuppressWarnings("PMD.LawOfDemeter")
+class AbstractArrayTest {
+
+  @Test
+  @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+  void leafAbstractArrayNodeEquals() {
+    EqualsVerifier.forClass(AbstractArray.class)
+        .withIgnoredFields(MOD_COUNT, LOCK, STRICT)
+        .withRedefinedSuperclass()
+        .withRedefinedSubclass(OrdArray.class)
+        .withIgnoredAnnotations(NonNull.class)
+        .verify();
+  }
+
+  @Test
+  @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+  void leafAbstractOrdArrayNodeEquals() {
+    EqualsVerifier.forClass(AbstractOrdArray.class)
+        .withIgnoredFields(MOD_COUNT, LOCK, STRICT)
+        .withRedefinedSuperclass()
+        .withRedefinedSubclass(OrdArray.class)
+        .withIgnoredAnnotations(NonNull.class)
+        .verify();
+  }
+}
