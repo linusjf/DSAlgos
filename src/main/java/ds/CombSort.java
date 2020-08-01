4a10c4fdb711597876f74a03f8bc576478986884
fernal73 <linus.fernandes@gmail.com>
2020-08-01 17:20:52 +0530

    Add CombSort

---
+++ src/main/java/ds/CombSort.java
@@ -0,0 +1,27 @@
+package ds;
+
+import static java.lang.Math.floor;
+
+public class CombSort extends AbstractSort {
+  private static final double GAP_SHRINK_FACTOR = 1.3d;
+
+  protected void sort(long[] a, int length) {
+    int gap = length;
+    boolean sorted = false;
+    while (!sorted) {
+      gap = (int) floor(gap / GAP_SHRINK_FACTOR);
+      if (gap <= 1) {
+        gap = 1;
+        sorted = true;
+      }
+      int i = 0;
+      while (i < gap + length) {
+        if (a[i] > a[i + gap]) {
+          swap(a, i, i + gap);
+          sorted = false;
+        }
+        ++i;
+      }
+    }
+  }
+}
