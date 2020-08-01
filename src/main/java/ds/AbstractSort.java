54b9a8b6f2779309cedd4fef6489ae311d8ca0f6
fernal73 <linus.fernandes@gmail.com>
2020-08-01 10:43:42 +0530

    Modify classes

--- src/main/java/ds/AbstractSort.java
+++ src/main/java/ds/AbstractSort.java
@@ -5,6 +5,9 @@ public abstract class AbstractSort implements ISort {
   @Override
   protected abstract void sort(long[] array, int length);
 
+  @Override
+  public abstract IArray sort(IArray array);
+
   protected void swap(long[] a, int first, int second) {
     long temp = a[first];
     a[first] = a[second];
--- src/main/java/ds/BubbleSort.java
+++ src/main/java/ds/BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort extends AbstractSort {
     return copy;
   }
 
-  private void sort(long[] a, int length) {
+  protected void sort(long[] a, int length) {
     int out, in;
     for (out = length - 1; out > 1; out--)
       for (in = 0; in < out; in++) if (a[in] > a[in + 1]) swap(a, in, in + 1);
