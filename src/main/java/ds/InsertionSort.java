3598e5ee6d66b976a7ca950be66055ed5d4b2215
fernal73 <linus.fernandes@gmail.com>
2020-08-03 09:52:19 +0530

    Optimise right shift

--- src/main/java/ds/InsertionSort.java
+++ src/main/java/ds/InsertionSort.java
@@ -12,11 +12,11 @@ public class InsertionSort extends AbstractSort {
       long temp = a[out];
       in = out;
       while (in > 0 && a[in - 1] >= temp) {
-        a[in] = a[in - 1];
         --in;
         ++comparisonCount;
         ++copyCount;
       }
+      System.arrayCopy(a, in, a, in + 1, copyCount);
       if (in > 0) ++comparisonCount;
       a[in] = temp;
       ++copyCount;
