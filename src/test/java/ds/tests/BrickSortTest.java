7dfdb9ddfcfdd6b386f042936e167b5fefb61514
fernal73 <linus.fernandes@gmail.com>
2020-08-03 17:47:28 +0530

    Add more tests to other algos

--- src/test/java/ds/tests/BrickSortTest.java
+++ src/test/java/ds/tests/BrickSortTest.java
@@ -4,8 +4,12 @@ import static ds.tests.TestData.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import ds.BrickSort;
+import ds.HighArray;
 import ds.IArray;
 import ds.ISort;
+import ds.OrdArray;
+import java.util.stream.LongStream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.parallel.Execution;
@@ -27,5 +31,40 @@ class BrickSortTest {
     IArray sorted = sorter.sort(arr);
     long[] extent = sorted.getExtentArray();
     assertArrayEquals(a, extent, "Elements must be sorted and equal.");
+  }
+
+  @Test
+  void testStreamUnSorted() {
+    IArray high = new HighArray();
+    IArray ord = new OrdArray();
+    LongStream.rangeClosed(1, 20)
+        .unordered()
+        .forEach(
+            i -> {
+              high.insert(i);
+              ord.insert(i);
+            });
+    ISort sorter = new BrickSort();
+    IArray sorted = sorter.sort(high);
+    long[] extentSorted = sorted.getExtentArray();
+    long[] extent = ord.getExtentArray();
+    assertArrayEquals(extentSorted, extent, "Elements must be sorted and equal.");
+  }
+
+  @Test
+  void testStreamSorted() {
+    IArray high = new HighArray();
+    IArray ord = new OrdArray();
+    LongStream.rangeClosed(1, 20)
+        .forEach(
+            i -> {
+              high.insert(i);
+              ord.insert(i);
+            });
+    ISort sorter = new BrickSort();
+    IArray sorted = sorter.sort(high);
+    long[] extentSorted = sorted.getExtentArray();
+    long[] extent = ord.getExtentArray();
+    assertArrayEquals(extentSorted, extent, "Elements must be sorted and equal.");
   }
 }
--- src/test/java/ds/tests/InsertionSortTest.java
+++ src/test/java/ds/tests/InsertionSortTest.java
@@ -3,9 +3,13 @@ package ds.tests;
 import static ds.tests.TestData.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import ds.HighArray;
 import ds.IArray;
 import ds.ISort;
 import ds.InsertionSort;
+import ds.OrdArray;
+import java.util.stream.LongStream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.parallel.Execution;
@@ -27,5 +31,40 @@ class InsertionSortTest {
     IArray sorted = sorter.sort(arr);
     long[] extent = sorted.getExtentArray();
     assertArrayEquals(a, extent, "Elements must be sorted and equal.");
+  }
+
+  @Test
+  void testStreamUnSorted() {
+    IArray high = new HighArray();
+    IArray ord = new OrdArray();
+    LongStream.rangeClosed(1, 20)
+        .unordered()
+        .forEach(
+            i -> {
+              high.insert(i);
+              ord.insert(i);
+            });
+    ISort sorter = new InsertionSort();
+    IArray sorted = sorter.sort(high);
+    long[] extentSorted = sorted.getExtentArray();
+    long[] extent = ord.getExtentArray();
+    assertArrayEquals(extentSorted, extent, "Elements must be sorted and equal.");
+  }
+
+  @Test
+  void testStreamSorted() {
+    IArray high = new HighArray();
+    IArray ord = new OrdArray();
+    LongStream.rangeClosed(1, 20)
+        .forEach(
+            i -> {
+              high.insert(i);
+              ord.insert(i);
+            });
+    ISort sorter = new InsertionSort();
+    IArray sorted = sorter.sort(high);
+    long[] extentSorted = sorted.getExtentArray();
+    long[] extent = ord.getExtentArray();
+    assertArrayEquals(extentSorted, extent, "Elements must be sorted and equal.");
   }
 }
