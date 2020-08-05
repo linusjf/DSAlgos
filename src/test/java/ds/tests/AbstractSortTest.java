0fe5436b17db3dc783f5703124008e21c9ae2971
fernal73 <linus.fernandes@gmail.com>
2020-08-05 06:01:39 +0530

    Test swap method for same index

--- src/test/java/ds/tests/AbstractSortTest.java
+++ src/test/java/ds/tests/AbstractSortTest.java
@@ -1,7 +1,13 @@
 package ds.tests;
 
-import static ds.tests.TestConstants.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+import ds.AbstractSort;
+import ds.HighArray;
+import ds.IArray;
+import ds.ISort;
+import java.util.stream.LongStream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.parallel.Execution;
@@ -11,5 +17,21 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 @Execution(ExecutionMode.SAME_THREAD)
 @SuppressWarnings("PMD.LawOfDemeter")
 class AbstractSortTest {
-  // empty class
+
+  static class ConcreteSort extends AbstractSort {
+    protected void sort(long[] a, int length) {
+      for (int i = 0; i < length; i++) swap(a, i, i);
+    }
+  }
+
+  @Test
+  void testConcreteSort() {
+    IArray high = new HighArray();
+    LongStream.rangeClosed(1, 20).parallel().unordered().forEach(i -> high.insert(i));
+    long[] arr = high.get();
+    ISort sorter = new ConcreteSort();
+    IArray sorted = sorter.sort(high);
+    long[] arr2 = sorted.get();
+    assertArrayEquals(arr, arr2, "Arrays must be equal");
+  }
 }
