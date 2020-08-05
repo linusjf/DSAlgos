06a1412144cc54f4c8c1c273d6d26f3a78ad1189
fernal73 <linus.fernandes@gmail.com>
2020-08-05 20:53:19 +0530

    Refactor BrickSort

--- src/main/java/ds/BrickSort.java
+++ src/main/java/ds/BrickSort.java
@@ -2,40 +2,52 @@ package ds;
 
 public class BrickSort extends AbstractSort {
 
+  private boolean isSorted;
+
+  private void reset() {
+    resetCounts();
+    isSorted = false;
+  }
+
   @Override
   protected void sort(long[] a, int length) {
-    boolean isSorted = false;
-    resetCounts();
+    reset();
     final int maxComparisons =
         (length & 1) == 1 ? length * ((length - 1) >>> 1) : (length >>> 1) * (length - 1);
     while (!isSorted) {
       ++outerLoopCount;
       isSorted = true;
-      // Perform Bubble sort on odd indexed element
-      for (int i = 1; i < length -1; i = i + 2) {
-        ++innerLoopCount;
-        ++comparisonCount;
-        if (a[i] > a[i + 1]) {
-          swap(a, i, i + 1);
-          isSorted = false;
-          ++swapCount;
-        }
-      }
+      oddSort(a, length);
       if (swapCount == maxComparisons) {
         isSorted = true;
         break;
       }
-      // Perform Bubble sort on even indexed element
-      for (int i = 0; i < length - 1; i = i + 2) {
-        ++innerLoopCount;
-        ++comparisonCount;
-        if (a[i] > a[i + 1]) {
-          swap(a, i, i + 1);
-          isSorted = false;
-          ++swapCount;
-        }
-      }
+      evenSort(a, length);
       if (swapCount == maxComparisons) isSorted = true;
+    }
+  }
+
+  private void oddSort(long[] a, int length) {
+    for (int i = 1; i < length - 1; i = i + 2) {
+      ++innerLoopCount;
+      ++comparisonCount;
+      bubble(a, i);
+    }
+  }
+
+  private void evenSort(long[] a, int length) {
+    for (int i = 0; i < length - 1; i = i + 2) {
+      ++innerLoopCount;
+      ++comparisonCount;
+      bubble(a, i);
+    }
+  }
+
+  protected void bubble(long[] a, int i) {
+    if (a[i] > a[i + 1]) {
+      swap(a, i, i + 1);
+      isSorted = false;
+      ++swapCount;
     }
   }
 }
