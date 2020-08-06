18aa501f6f37dec0493e5a3f966cbe37622d1fe0
fernal73 <linus.fernandes@gmail.com>
2020-08-06 08:31:37 +0530

    Make BrickSortParallel

---
+++ src/main/java/ds/BrickSortParallel.java
@@ -0,0 +1,113 @@
+package ds;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class BrickSortParallel extends AbstractSort {
+
+  private AtomicBoolean isSorted = new AtomicBoolean();
+  private AtomicInteger swapCount = new AtomicInteger();
+  private ExecutorService service;
+
+  private void reset() {
+    resetCounts();
+    isSorted.getAndSet(false);
+    swapCount.set(0);
+  }
+
+  @Override
+  protected void sort(long[] a, int length) {
+    reset();
+    final int maxComparisons =
+        (length & 1) == 1 ? length * ((length - 1) >>> 1) : (length >>> 1) * (length - 1);
+    while (!isSorted.get()) {
+      ++outerLoopCount;
+      isSorted.set(true);
+      oddSort(a, length);
+      if (swapCount.intValue() == maxComparisons) {
+        isSorted.set(true);
+        break;
+      }
+      evenSort(a, length);
+      if (swapCount.intValue() == maxComparisons) isSorted.set(true);
+    }
+  }
+
+  private void oddSort(long[] a, int length) {
+    service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    List<Future<Void>> futures = new ArrayList<>(length - 2);
+
+    for (int i = 1; i < length - 1; i = i + 2) {
+      ++innerLoopCount;
+      ++comparisonCount;
+      futures.add(service.submit(new Task(this, a, i)));
+    }
+    try {
+      service.shutdown();
+      service.awaitTermination(length, TimeUnit.MILLISECONDS);
+      for (int i = 0; i < futures.size(); i++) futures.get(i).get();
+    } catch (InterruptedException ie) {
+      Thread.currentThread().interrupt();
+    } catch (ExecutionException ee) {
+      System.err.println(ee.getMessage());
+    }
+  }
+
+  private void evenSort(long[] a, int length) {
+    service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    List<Future<Void>> futures = new ArrayList<>(length - 1);
+    for (int i = 0; i < length - 1; i = i + 2) {
+      ++innerLoopCount;
+      ++comparisonCount;
+      futures.add(service.submit(new Task(this, a, i)));
+    }
+
+    try {
+      service.shutdown();
+      service.awaitTermination(length, TimeUnit.MILLISECONDS);
+      for (int i = 0; i < futures.size(); i++) futures.get(i).get();
+    } catch (InterruptedException ie) {
+      Thread.currentThread().interrupt();
+    } catch (ExecutionException ee) {
+      System.err.println(ee.getMessage());
+    }
+  }
+
+  protected void bubble(long[] a, int i) {
+    if (a[i] > a[i + 1]) {
+      swap(a, i, i + 1);
+      isSorted.set(false);
+      swapCount.incrementAndGet();
+    }
+  }
+
+  @Override
+  public int getSwapCount() {
+    return swapCount.intValue();
+  }
+
+  static class Task implements Callable<Void> {
+    long[] a;
+    int i;
+    BrickSortParallel sorter;
+
+    Task(BrickSortParallel sorter, long[] a, int i) {
+      this.a = a;
+      this.i = i;
+      this.sorter = sorter;
+    }
+
+    public Void call() {
+      sorter.bubble(a, i);
+      return null;
+    }
+  }
+}
---
+++ src/test/java/ds/tests/BrickSortParallelTest.java
@@ -0,0 +1,136 @@
+package ds.tests;
+
+import static ds.ArrayUtils.*;
+import static ds.tests.TestData.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import ds.BrickSortParallel;
+import ds.HighArray;
+import ds.IArray;
+import ds.ISort;
+import ds.OrdArray;
+import java.util.stream.LongStream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@Execution(ExecutionMode.SAME_THREAD)
+@SuppressWarnings("PMD.LawOfDemeter")
+class BrickSortParallelTest implements SortProvider {
+
+  @ParameterizedTest
+  @CsvSource(INIT_DATA)
+  void testSort(@AggregateWith(HighArrayArgumentsAggregator.class) IArray arr) {
+    long[] a = {00, 11, 22, 33, 44, 55, 66, 77, 88, 99};
+    ISort sorter = new BrickSortParallel();
+    IArray sorted = sorter.sort(arr);
+    long[] extent = sorted.getExtentArray();
+    assertArrayEquals(a, extent, "Elements must be sorted and equal.");
+  }
+
+  @Test
+  void testStreamUnSorted() {
+    IArray high = new HighArray();
+    IArray ord = new OrdArray();
+    LongStream.rangeClosed(1, 20)
+        .unordered()
+        .forEach(
+            i -> {
+              high.insert(i);
+              ord.insert(i);
+            });
+    ISort sorter = new BrickSortParallel();
+    IArray sorted = sorter.sort(high);
+    long[] extentSorted = sorted.getExtentArray();
+    long[] extent = ord.getExtentArray();
+    assertArrayEquals(extentSorted, extent, "Elements must be sorted and equal.");
+  }
+
+  @Test
+  void testStreamSorted() {
+    IArray high = new HighArray();
+    IArray ord = new OrdArray();
+    LongStream.rangeClosed(1, 20)
+        .forEach(
+            i -> {
+              high.insert(i);
+              ord.insert(i);
+            });
+    ISort sorter = new BrickSortParallel();
+    IArray sorted = sorter.sort(high);
+    long[] extentSorted = sorted.getExtentArray();
+    long[] extent = ord.getExtentArray();
+    assertArrayEquals(extentSorted, extent, "Elements must be sorted and equal.");
+  }
+
+  @Test
+  void testComparisonCountSorted() {
+    IArray high = new HighArray();
+    LongStream.rangeClosed(1, 20).forEach(i -> high.insert(i));
+    ISort sorter = new BrickSortParallel();
+    IArray sorted = sorter.sort(high);
+    int compCount = sorter.getComparisonCount();
+    assertEquals(19, compCount, "Comparison count must be 19.");
+  }
+
+  @Test
+  void testComparisonCountUnsorted() {
+    IArray high = new HighArray();
+    LongStream.rangeClosed(1, 20).parallel().unordered().forEach(i -> high.insert(i));
+    ISort sorter = new BrickSortParallel();
+    IArray sorted = sorter.sort(high);
+    int compCount = sorter.getComparisonCount();
+    assertTrue(
+        19 <= compCount && compCount <= 400, "Comparison count must be in range 19 and 400.");
+  }
+
+  @Test
+  void testReverseSorted() {
+    IArray high = new HighArray();
+    revRange(1, 20).forEach(i -> high.insert(i));
+    ISort sorter = new BrickSortParallel();
+    IArray sorted = sorter.sort(high);
+    assertEquals(
+        sorter.getSwapCount(),
+        sorter.getComparisonCount(),
+        "Comparison count must be same as swap count in reverse ordered array.");
+    assertTrue(isSorted(sorted), "Array must be sorted");
+  }
+
+  @Test
+  void testReverseSortedOdd() {
+    IArray high = new HighArray();
+    revRange(1, 21).forEach(i -> high.insert(i));
+    ISort sorter = new BrickSortParallel();
+    IArray sorted = sorter.sort(high);
+    assertEquals(
+        sorter.getSwapCount(),
+        sorter.getComparisonCount(),
+        "Comparison count must be same as swap count in reverse ordered array.");
+    assertTrue(isSorted(sorted), "Array must be sorted");
+  }
+
+  @Test
+  void testSwapCount() {
+    IArray high = new HighArray();
+    LongStream.rangeClosed(1, 20).forEach(i -> high.insert(i));
+    ISort sorter = new BrickSortParallel();
+    IArray sorted = sorter.sort(high);
+    assertEquals(0, sorter.getSwapCount(), "Swap count must be zero.");
+  }
+
+  @Test
+  void testTimeComplexity() {
+    IArray high = new HighArray();
+    LongStream.rangeClosed(1, 20).forEach(i -> high.insert(i));
+    ISort sorter = new BrickSortParallel();
+    IArray sorted = sorter.sort(high);
+    assertEquals(19, sorter.getTimeComplexity(), "Time complexity must be twenty.");
+  }
+}
