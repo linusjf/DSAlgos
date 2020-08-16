3943be66def1230dd5d51be657e705f1d6ad6c48
fernal73 <linus.fernandes@gmail.com>
2020-08-16 10:34:36 +0530

    Remove reflection unwanted

--- src/test/java/ds/tests/TestUtils.java
+++ src/test/java/ds/tests/TestUtils.java
@@ -2,20 +2,21 @@ package ds.tests;
 
 import static org.joor.Reflect.*;
 
-import java.lang.reflect.Field;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-import org.joor.Reflect;
 
 @SuppressWarnings("PMD.LawOfDemeter")
 public final class TestUtils {
   static {
-    Field.class.getModule().addOpens(Field.class.getPackageName(), Reflect.class.getModule());
-    ReentrantReadWriteLock.WriteLock.class
-        .getModule()
-        .addOpens(
-            ReentrantReadWriteLock.WriteLock.class.getPackageName(),
-            nl.jqno.equalsverifier.internal.reflection.FieldAccessor.class.getModule());
+    try {
+      Class<?> syncClass = Class.forName("java.util.concurrent.locks.ReentrantReadWriteLock.Sync");
+      syncClass
+          .getModule()
+          .addOpens(
+              syncClass.getPackageName(),
+              nl.jqno.equalsverifier.internal.reflection.FieldAccessor.class.getModule());
+    } catch (ClassNotFoundException cnfe) {
+      System.err.println(cnfe.getMessage());
+    }
   }
 
   private TestUtils() {
