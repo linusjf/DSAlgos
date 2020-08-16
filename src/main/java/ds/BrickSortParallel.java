bd1c6e3fa91000f031a66f4f55e692128f14ef8b
fernal73 <linus.fernandes@gmail.com>
2020-08-16 06:00:15 +0530

    Change conditional boundaries. upgrade mockito

--- src/main/java/ds/BrickSortParallel.java
+++ src/main/java/ds/BrickSortParallel.java
@@ -152,12 +152,12 @@ public class BrickSortParallel extends AbstractBrickSort {
 
   public static int computeOddTaskCount(int length) {
     if (length < 0) throw new IllegalArgumentException("Illegal argument value: " + length);
-    return length < 2 ? 0 : isOdd(length) ? length >> 1 : (length - 1) >> 1;
+    return length <= 2 ? 0 : isOdd(length) ? length >> 1 : (length - 1) >> 1;
   }
 
   public static int computeEvenTaskCount(int length) {
     if (length < 0) throw new IllegalArgumentException("Illegal argument value: " + length);
-    return length > 0 ? length >> 1 : 0;
+    return length >= 0 ? length >> 1 : 0;
   }
 
   static final class BubbleTask implements Callable<Void> {
