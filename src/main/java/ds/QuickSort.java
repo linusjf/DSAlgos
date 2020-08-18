81f8db0d700ca674c867dba1ab0ff29775933f92
fernal73 <linus.fernandes@gmail.com>
2020-08-18 10:42:32 +0530

    Add QuickSortTest

--- src/main/java/ds/QuickSort.java
+++ src/main/java/ds/QuickSort.java
@@ -2,6 +2,7 @@ package ds;
 
 import java.util.Random;
 
+/** Quick sort using random partitioning */
 public class QuickSort extends AbstractSort {
 
   private static int randomInRange(int low, int high) {
@@ -11,46 +12,60 @@ public class QuickSort extends AbstractSort {
 
   @Override
   protected void sort(long[] a, int length) {
-    quickSort(a, 0, length);
+    quickSort(a, 0, length - 1);
   }
 
   private int randomPartition(long[] a, int low, int high) {
-    int random = randomInRange(low, high);
-    swap(a, random, high);
+    int random = randomInRange(low, high + 1);
+    printRandom(random);
+    if (random != high) {
+      swap(a, random, high);
+      ++swapCount;
+    }
     return partition(a, low, high);
   }
 
-  /* This function takes last element as pivot, places
+  /*
+   * This function takes last element as pivot, places
   the pivot element at its correct position in sorted
   array, and places all smaller (smaller than pivot)
   to left of pivot and all greater elements to right
-  of pivot */
+  of pivot
+  */
   int partition(long[] a, int low, int high) {
     long pivot = a[high];
 
     int i = low - 1;
     // Index of smaller element
-    for (int j = low; j <= high - 1; ++j) {
+    for (int j = low; j < high; ++j) {
       // If current element is smaller than or
       // equal to pivot
       ++innerLoopCount;
       ++comparisonCount;
       if (a[j] <= pivot) {
         ++i;
-        swap(a, i, j);
-        ++swapCount;
+        if (i != j) {
+          swap(a, i, j);
+          ++swapCount;
+        }
       }
     }
-    swap(a, i + 1, high);
-    ++swapCount;
-    return i + 1;
+    ++i;
+    if (i != high) {
+      swap(a, i, high);
+      ++swapCount;
+    }
+    return i;
   }
 
-  /* This QuickSort requires O(Log n) auxiliary space in
-  worst case. */
+  /*
+   * This QuickSort requires O(Log n) auxiliary space in worst case.
+   */
   void quickSort(long[] a, int low, int high) {
     while (low < high) {
       ++outerLoopCount;
+      printSubArray(a, low, high);
+      printThis();
       /* pi is partitioning index, a[p] is now
       at right place */
       int pi = randomPartition(a, low, high);
@@ -64,5 +79,18 @@ public class QuickSort extends AbstractSort {
         high = pi - 1;
       }
     }
+  }
+
+  private void printSubArray(long[] a, int start, int end) {
+    for (int i = start; i <= end; i++) System.out.printf("%d ", a[i]);
+    System.out.println("");
+  }
+
+  private void printThis() {
+    System.out.println(this);
+  }
+
+  private void printRandom(int num) {
+    System.out.printf("%d %n", num);
   }
 }
---
+++ src/test/java/ds/tests/QuickSortTest.java
@@ -0,0 +1,202 @@
+package ds.tests;
+
+import static ds.ArrayUtils.*;
+import static ds.tests.TestConstants.*;
+import static ds.tests.TestData.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import ds.AbstractSort;
+import ds.HighArray;
+import ds.IArray;
+import ds.ISort;
+import ds.OrdArray;
+import ds.QuickSort;
+import java.util.stream.LongStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@Execution(ExecutionMode.SAME_THREAD)
+@DisplayName("QuickSortTest")
+@SuppressWarnings("PMD.LawOfDemeter")
+class QuickSortTest implements SortProvider {
+
+
+  @ParameterizedTest
+  @CsvSource(INIT_DATA)
+  @DisplayName("QuickSortTest.testSort")
+  void testSort(@AggregateWith(HighArrayArgumentsAggregator.class) IArray arr) {
+    long[] a = {00, 11, 22, 33, 44, 55, 66, 77, 88, 99};
+    ISort sorter = new QuickSort();
+    IArray sorted = sorter.sort(arr);
+    long[] extent = sorted.getExtentArray();
+    assertArrayEquals(a, extent, ELEMENTS_SORTED_EQUAL);
+  }
+
+  @ParameterizedTest
+  @CsvSource(INIT_DUPLICATE_DATA)
+  @DisplayName("QuickSortTest.testSortDuplicates")
+  void testSortDuplicates(@AggregateWith(HighArrayArgumentsAggregator.class) IArray arr) {
+    long[] a = {00, 00, 00, 00, 11, 11, 11, 22, 22, 33, 33, 44, 55, 66, 77, 77, 77, 88, 88, 99, 99};
+    ISort sorter = new QuickSort();
+    IArray sorted = sorter.sort(arr);
+    long[] extent = sorted.getExtentArray();
+    assertArrayEquals(a, extent, ELEMENTS_SORTED_EQUAL);
+  }
+
+  @ParameterizedTest
+  @CsvSource(INIT_INSERTION_SORT_DATA)
+  @DisplayName("QuickSortTest.testSortSmallData")
+  void testSortSmallData(@AggregateWith(HighArrayArgumentsAggregator.class) IArray arr) {
+    ISort sorter = new QuickSort();
+    OrdArray ord = new OrdArray();
+    long[] a = arr.getExtentArray();
+    for (int i = 0; i < arr.count(); i++) ord.insert(a[i]);
+    a = ord.getExtentArray();
+    IArray sorted = sorter.sort(arr);
+    long[] internal = sorted.getExtentArray();
+    assertArrayEquals(a, internal, "Arrays must be sorted and equal.");
+    assertTrue(isSorted(sorted), "Array must be sorted.");
+  }
+
+  @ParameterizedTest
+  @CsvSource(INIT_ALL_SAME_DATA)
+  @DisplayName("QuickSortTest.testSortAllSame")
+  void testSortAllSame(@AggregateWith(HighArrayArgumentsAggregator.class) IArray arr) {
+    long[] a = {43, 43, 43, 43, 43, 43, 43, 43, 43, 43};
+    ISort sorter = new QuickSort();
+    IArray sorted = sorter.sort(arr);
+    long[] extent = sorted.getExtentArray();
+    assertArrayEquals(a, extent, ELEMENTS_SORTED_EQUAL);
+  }
+
+  @Test
+  @DisplayName("QuickSortTest.testReset")
+  void testReset() {
+    IArray high = new HighArray();
+    IArray ord = new OrdArray();
+    LongStream.rangeClosed(1, 20)
+        .forEach(
+            i -> {
+              high.insert(i);
+              ord.insert(i);
+            });
+    ISort sorter = new QuickSort();
+    sorter.sort(high);
+    sorter.sort(ord);
+    int comparisonCount = sorter.getComparisonCount();
+    assertTrue(
+        20 * Math.log(20) < comparisonCount && comparisonCount <= 20 * 19,
+        "Comparison count must be in range nlogn to n(n-1)");
+  }
+
+  @Test
+  @DisplayName("QuickSortTest.testStreamUnsorted")
+  void testStreamUnSorted() {
+    IArray high = new HighArray();
+    IArray ord = new OrdArray();
+    LongStream.rangeClosed(1, 20)
+        .unordered()
+        .forEach(
+            i -> {
+              high.insert(i);
+              ord.insert(i);
+            });
+    ISort sorter = new QuickSort();
+    IArray sorted = sorter.sort(high);
+    long[] extentSorted = sorted.getExtentArray();
+    long[] extent = ord.getExtentArray();
+    assertArrayEquals(extentSorted, extent, ELEMENTS_SORTED_EQUAL);
+  }
+
+  @Test
+  @DisplayName("QuickSortTest.testStreamSorted")
+  void testStreamSorted() {
+    IArray high = new HighArray();
+    IArray ord = new OrdArray();
+    LongStream.rangeClosed(1, 20)
+        .forEach(
+            i -> {
+              high.insert(i);
+              ord.insert(i);
+            });
+    ISort sorter = new QuickSort();
+    IArray sorted = sorter.sort(high);
+    long[] extentSorted = sorted.getExtentArray();
+    long[] extent = ord.getExtentArray();
+    assertArrayEquals(extentSorted, extent, ELEMENTS_SORTED_EQUAL);
+  }
+
+  @Test
+  @DisplayName("QuickSortTest.testSwapCount")
+  void testSwapCount() {
+    IArray high = new HighArray();
+    IArray ord = new OrdArray();
+    LongStream.rangeClosed(1, 20)
+        .forEach(
+            i -> {
+              high.insert(i);
+              ord.insert(i);
+            });
+    ISort sorter = new QuickSort();
+    sorter.sort(high);
+    assertNotEquals(0, sorter.getSwapCount(), "Swap count must be zero.");
+  }
+
+  @Test
+  @DisplayName("QuickSortTest.testTimeComplexity")
+  void testTimeComplexity() {
+    IArray high = new HighArray();
+    LongStream.rangeClosed(1, 20).forEach(i -> high.insert(i));
+    ISort sorter = new QuickSort();
+    sorter.sort(high);
+    assertNotEquals(0, sorter.getTimeComplexity(), "Time complexity must be 248.");
+  }
+
+  @Test
+  @DisplayName("QuickSortTest.testTimeComplexityReverseSorted")
+  void testTimeComplexityReverseSorted() {
+    IArray high = new HighArray();
+    revRange(1, 20).forEach(i -> high.insert(i));
+    ISort sorter = new QuickSort();
+    sorter.sort(high);
+    assertNotEquals(0, sorter.getTimeComplexity(), "Time complexity must be thirty six.");
+  }
+
+  @Test
+  @DisplayName("QuickSortTest.testReverseSorted")
+  void testReverseSorted() {
+    IArray high = new HighArray();
+    revRange(1, 20).forEach(i -> high.insert(i));
+    ISort sorter = new QuickSort();
+    sorter.sort(high);
+    assertNotEquals(0, sorter.getSwapCount(), "Swap count must be 20 in reverse ordered array.");
+  }
+
+  @Test
+  @DisplayName("QuickSortTest.testToStringClass")
+  void testToStringClass() {
+    AbstractSort sorter = new QuickSort();
+    String className = QuickSort.class.getName();
+    assertTrue(
+        sorter.toString().startsWith(className), () -> "ToString must start with " + className);
+  }
+
+  @Test
+  @DisplayName("QuickSortTest.testPreReset")
+  void testPreReset() {
+    ISort sorter = new QuickSort();
+    assertEquals(0, sorter.getComparisonCount(), INITIAL_VALUE_ZERO);
+    assertEquals(0, sorter.getCopyCount(), INITIAL_VALUE_ZERO);
+    assertEquals(0, sorter.getTimeComplexity(), INITIAL_VALUE_ZERO);
+    assertEquals(0, sorter.getCopyCount(), INITIAL_VALUE_ZERO);
+  }
+}
