2fd93408a8568dd08db52940f41e165c9eabb801
fernal73 <linus.fernandes@gmail.com>
2020-08-19 11:40:07 +0530

    Add Display Name

--- src/test/java/ds/tests/MathUtilsTest.java
+++ src/test/java/ds/tests/MathUtilsTest.java
@@ -3,6 +3,7 @@ package ds.tests;
 import static ds.MathUtils.isOdd;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
@@ -12,39 +13,47 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 @TestInstance(Lifecycle.PER_CLASS)
 @Execution(ExecutionMode.SAME_THREAD)
 @SuppressWarnings({"PMD.LawOfDemeter", "PMD.DataflowAnomalyAnalysis"})
+@DisplayName("MathUtilsTest")
 class MathUtilsTest {
 
   @Test
+@DisplayName("MathUtilsTest.testZero")
   void testZero() {
     assertFalse(isOdd(0), "Zero is even!");
   }
 
   @Test
+@DisplayName("MathUtilsTest.testOne")
   void testOne() {
     assertTrue(isOdd(1), "One is odd!");
   }
 
   @Test
+@DisplayName("MathUtilsTest.testMinusOne")
   void testMinusOne() {
     assertTrue(isOdd(-1), "Minus One is odd!");
   }
 
   @Test
+@DisplayName("MathUtilsTest.testTwo")
   void testTwo() {
     assertFalse(isOdd(2), "Two is even!");
   }
 
   @Test
+@DisplayName("MathUtilsTest.testMinusTwo")
   void testMinusTwo() {
     assertFalse(isOdd(-2), "Minus Two is even!");
   }
 
   @Test
+@DisplayName("MathUtilsTest.testMaxValue")
   void testMaxValue() {
     assertTrue(isOdd(Integer.MAX_VALUE), () -> Integer.MAX_VALUE + " is odd!");
   }
 
   @Test
+@DisplayName("MathUtilsTest.testMinValue")
   void testMinValue() {
     assertFalse(isOdd(Integer.MIN_VALUE), () -> Integer.MIN_VALUE + " is even!");
   }
