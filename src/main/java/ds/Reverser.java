8250961f08759c8add173f67fa08b17e6c70548d
fernal73 <linus.fernandes@gmail.com>
2020-08-23 17:49:55 +0530

    Add Reverser

---
+++ src/main/java/ds/Reverser.java
@@ -0,0 +1,24 @@
+package ds;
+
+public class Reverser {
+  private String input;
+
+  public Reverser(String in) {
+    input = in;
+  }
+
+  public String doRev() {
+    int stackSize = input.length();
+    SimpleStack theStack = new SimpleStack(stackSize);
+    for (int j = 0; j < stackSize; j++) {
+      char ch = input.charAt(j);
+      theStack.push(ch);
+    }
+    StringBuilder sb = new StringBuilder();
+    while (!theStack.isEmpty()) {
+      char ch = (char) theStack.pop();
+      sb.append(ch);
+    }
+    return sb.toString();
+  }
+}
