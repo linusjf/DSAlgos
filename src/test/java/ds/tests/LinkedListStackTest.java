4bfc679334f655d428a0325afa46263fefe1a996
fernal73 <linus.fernandes@gmail.com>
2020-08-24 09:41:55 +0530

    Add LinkedListStackTest

--- src/test/java/ds/tests/LinkedListStackTest.java
+++ src/test/java/ds/tests/LinkedListStackTest.java
@@ -3,7 +3,7 @@ package ds.tests;
 import static org.junit.jupiter.api.Assertions.*;
 
 import ds.IStack;
-import ds.SimpleStack;
+import ds.LinkedListStack;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -11,130 +11,130 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
-@DisplayName("SimpleStackTest")
+@DisplayName("LinkedListStackTest")
 @TestInstance(Lifecycle.PER_CLASS)
 @Execution(ExecutionMode.SAME_THREAD)
-class SimpleStackTest {
+class LinkedListStackTest {
 
   private static final long VAL = 20;
 
   @Test
-  @DisplayName("SimpleStackTest.testPopEmpty")
+  @DisplayName("LinkedListStackTest.testPopEmpty")
   void testPopEmpty() {
-    IStack stack = new SimpleStack(0);
+    IStack stack = new LinkedListStack();
     assertThrows(
         ArrayIndexOutOfBoundsException.class,
         () -> stack.pop(),
         "Empty stack pop throws exception.");
   }
 
-  @DisplayName("SimpleStackTest.testPopEmptyOne")
+  @DisplayName("LinkedListStackTest.testPopEmptyOne")
   @Test
   void testPopEmptyOne() {
-    IStack stack = new SimpleStack(1);
+    IStack stack = new LinkedListStack();
     assertThrows(
         ArrayIndexOutOfBoundsException.class,
         () -> stack.pop(),
         "Empty stack pop throws exception.");
   }
 
-  @DisplayName("SimpleStackTest.testPop")
+  @DisplayName("LinkedListStackTest.testPop")
   @Test
   void testPop() {
-    IStack stack = new SimpleStack(1);
+    IStack stack = new LinkedListStack();
     long val = VAL;
     stack.push(val);
     assertEquals(val, stack.pop(), "Pop returns last value pushed.");
   }
 
-  @DisplayName("SimpleStackTest.testPush")
+  @DisplayName("LinkedListStackTest.testPush")
   @Test
   void testPushSizeOne() {
-    IStack stack = new SimpleStack(1);
+    IStack stack = new LinkedListStack();
     long val = VAL;
     stack.push(val);
     assertEquals(val, stack.peek(), "Pop returns last value pushed.");
   }
 
-  @DisplayName("SimpleStackTest.testPushException")
+  @DisplayName("LinkedListStackTest.testPushException")
   @Test
   void testPushException() {
-    IStack stack = new SimpleStack(1);
+    IStack stack = new LinkedListStack();
     long val = VAL;
     stack.push(val);
     assertThrows(
         ArrayIndexOutOfBoundsException.class, () -> stack.push(val), "Push throws exception.");
   }
 
-  @DisplayName("SimpleStackTest.testPushZeroSizeException")
+  @DisplayName("LinkedListStackTest.testPushZeroSizeException")
   @Test
   void testPushZeroSizeException() {
-    IStack stack = new SimpleStack(0);
+    IStack stack = new LinkedListStack();
     long val = VAL;
     assertThrows(
         ArrayIndexOutOfBoundsException.class, () -> stack.push(val), "Push throws exception.");
   }
 
-  @DisplayName("SimpleStackTest.testIsEmpty")
+  @DisplayName("LinkedListStackTest.testIsEmpty")
   @Test
   void testIsEmpty() {
-    IStack stack = new SimpleStack(0);
+    IStack stack = new LinkedListStack();
     assertTrue(stack.isEmpty(), "Stack must be empty.");
   }
 
-  @DisplayName("SimpleStackTest.testIsNotEmpty")
+  @DisplayName("LinkedListStackTest.testIsNotEmpty")
   @Test
   void testIsNotEmpty() {
-    IStack stack = new SimpleStack(1);
+    IStack stack = new LinkedListStack();
     stack.push(VAL);
     assertFalse(stack.isEmpty(), "Stack must not be empty.");
   }
 
-  @DisplayName("SimpleStackTest.testIsEmptySizeOne")
+  @DisplayName("LinkedListStackTest.testIsEmptySizeOne")
   @Test
   void testIsEmptySizeOne() {
-    IStack stack = new SimpleStack(1);
+    IStack stack = new LinkedListStack();
     assertTrue(stack.isEmpty(), "Stack must be empty.");
   }
 
-  @DisplayName("SimpleStackTest.testIsFull")
+  @DisplayName("LinkedListStackTest.testIsFull")
   @Test
   void testIsFull() {
-    IStack stack = new SimpleStack(0);
+    IStack stack = new LinkedListStack();
     assertTrue(stack.isFull(), "Stack must be full.");
   }
 
-  @DisplayName("SimpleStackTest.testIsFullSizeOne")
+  @DisplayName("LinkedListStackTest.testIsFullSizeOne")
   @Test
   void testIsFullSizeOne() {
-    IStack stack = new SimpleStack(1);
+    IStack stack = new LinkedListStack();
     assertFalse(stack.isFull(), "Stack must be empty.");
   }
 
   @Test
-  @DisplayName("SimpleStackTest.testPeekEmpty")
+  @DisplayName("LinkedListStackTest.testPeekEmpty")
   void testPeekEmpty() {
-    IStack stack = new SimpleStack(0);
+    IStack stack = new LinkedListStack();
     assertThrows(
         ArrayIndexOutOfBoundsException.class,
         () -> stack.peek(),
         "Empty stack peep throws exception.");
   }
 
-  @DisplayName("SimpleStackTest.testPeekEmptyOne")
+  @DisplayName("LinkedListStackTest.testPeekEmptyOne")
   @Test
   void testPeekEmptyOne() {
-    IStack stack = new SimpleStack(1);
+    IStack stack = new LinkedListStack();
     assertThrows(
         ArrayIndexOutOfBoundsException.class,
         () -> stack.peek(),
         "Empty stack peek throws exception.");
   }
 
-  @DisplayName("SimpleStackTest.testPeek")
+  @DisplayName("LinkedListStackTest.testPeek")
   @Test
   void testPeek() {
-    IStack stack = new SimpleStack(1);
+    IStack stack = new LinkedListStack();
     long val = VAL;
     stack.push(val);
     assertEquals(val, stack.peek(), "Peek returns last value pushed.");
