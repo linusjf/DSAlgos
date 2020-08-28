fe2a87ec10aa1d961becf934cdd94b922dd6e298
fernal73 <linus.fernandes@gmail.com>
2020-08-28 05:54:28 +0530

    Add assert method

--- src/main/java/ds/ExecutorUtils.java
+++ src/main/java/ds/ExecutorUtils.java
@@ -16,5 +16,12 @@ public final class ExecutorUtils {
     } catch (InterruptedException ie) {
       service.shutdownNow();
     }
+    assertServiceTerminated(service);
+  }
+
+  @Generated
+  private static void assertServiceTerminated(ExecutorService service) {
+    if (!service.isTerminated())
+      throw new AssertionError("ExecutorService must terminate cleanly.");
   }
 }
