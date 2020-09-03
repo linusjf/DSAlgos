0eda105c378105ee02a2dc46ba43279b4ff50169
fernal73 <linus.fernandes@gmail.com>
2020-09-03 19:25:03 +0530

    convert logic to minheap

--- src/main/java/ds/MinHeap.java
+++ src/main/java/ds/MinHeap.java
@@ -12,7 +12,7 @@ public class MinHeap extends AbstractHeap {
   protected void heapifyUp(int i) {
     if (i == 0) return;
     int parent = parent(i);
-    if (a[parent] < a[i]) {
+    if (a[parent] > a[i]) {
       swap(i, parent);
       heapifyUp(parent);
     }
@@ -22,14 +22,14 @@ public class MinHeap extends AbstractHeap {
   protected void heapifyDown(int i) {
     int left = leftChild(i);
     int right = rightChild(i);
-    int largest = i;
-    if (left < nElems && a[left] > a[i]) largest = left;
-    if (right < nElems && a[right] > a[largest]) largest = right;
-    if (largest != i) {
-      // swap with child having greater value
-      swap(i, largest);
+    int smallest = i;
+    if (left < nElems && a[left] < a[i]) smallest = left;
+    if (right < nElems && a[right] < a[smallest]) smallest = right;
+    if (smallest != i) {
+      // swap with child having lesser value
+      swap(i, smallest);
       // call heapify-down on the child
-      heapifyDown(largest);
+      heapifyDown(smallest);
     }
   }
 }
