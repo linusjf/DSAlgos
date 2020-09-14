9f4a7c100ab302877a6fb77791fe42b83cc28479
fernal73 <linus.fernandes@gmail.com>
2020-09-14 12:15:00 +0530

    Fix pmd violations

--- src/main/java/ds/Deque.java
+++ src/main/java/ds/Deque.java
@@ -47,7 +47,9 @@ public class Deque implements IQueue, IStack {
 
   @Override
   public boolean isFull() {
-    return arr.length == 0 ? true : first == 0 && last == arr.length - 1 ? true : first == last + 1;
+    return arr.length == 0 
+      || first == 0 && last == arr.length - 1 
+      || first == last + 1;
   }
 
   @Override
