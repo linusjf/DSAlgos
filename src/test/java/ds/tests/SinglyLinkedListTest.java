549ac183ac0de6c25568c261cd7292fd91b774c2
fernal73 <linus.fernandes@gmail.com>
2020-09-23 06:04:34 +0530

    Format. Start LinkedList tests

---
+++ src/test/java/ds/tests/SinglyLinkedListTest.java
@@ -0,0 +1,26 @@
+package ds.tests;
+
+import static ds.tests.TestConstants.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import ds.SinglyLinkedList;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
+@DisplayName("SinglyLinkedListTest")
+@TestInstance(Lifecycle.PER_CLASS)
+@Execution(ExecutionMode.SAME_THREAD)
+class SinglyLinkedListTest {
+
+  @Test
+  @DisplayName("SinglyLinkedListTest.testConstructor")
+  void testConstructor() {
+    SinglyLinkedList list = new SinglyLinkedList();
+    assertEquals(0, list.size(), "Size must be zero.");
+    assertNull(list.getHead(), "List head must be null.");
+  }
+}
