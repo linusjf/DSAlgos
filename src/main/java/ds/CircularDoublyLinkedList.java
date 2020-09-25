506627e6b22b61eae285faaee2450bb041ffc300
fernal73 <linus.fernandes@gmail.com>
2020-09-26 05:28:16 +0530

    Start CircularDoublyLinkedList

---
+++ src/main/java/ds/CircularDoublyLinkedList.java
@@ -0,0 +1,190 @@
+package ds;
+
+import java.util.Objects;
+
+public class CircularDoublyLinkedList<T> implements IList<T> {
+
+  private static final String DATA_NON_NULL = "Data cannot be null.";
+  private int length;
+
+  @SuppressWarnings("initialization.fields.uninitialized")
+  private INode<T> head;
+
+  @SuppressWarnings("initialization.fields.uninitialized")
+  private INode<T> tail;
+
+  /**
+   * Add element at end.
+   *
+   * @param data - data to be added to list.
+   */
+  @SuppressWarnings({"PMD.LawOfDemeter", "nullness:argument.type.incompatible"})
+  @Override
+  public void add(T data) {
+    Objects.requireNonNull(data, DATA_NON_NULL);
+    if (head == null) {
+      head = new DoubleNode<>(data);
+      head.setNext(head);
+      head.setPrev(head);
+      tail = head;
+    } else {
+      INode<T> newNode = new DoubleNode<>(data);
+      tail.setNext(newNode);
+      newNode.setPrev(tail);
+      newNode.setNext(head);
+      tail = newNode;
+    }
+    ++length;
+  }
+
+  /**
+   * Add the element at specified index.
+   *
+   * @param data - data to be added at index.
+   * @param index - index at which element to be added.
+   */
+  @SuppressWarnings({"PMD.LawOfDemeter", "nullness:argument.type.incompatible"})
+  @Override
+  public void add(T data, int index) {
+    Objects.requireNonNull(data, DATA_NON_NULL);
+    if (index == 0) {
+      addAtFirst(data);
+      return;
+    }
+    if (index == this.length) add(data);
+    else if (index < this.length) {
+      INode<T> newNode = new DoubleNode<>(data);
+      INode<T> leftNode = get(index - 1);
+      INode<T> rightNode = get(index);
+      newNode.setNext(rightNode);
+      newNode.setPrev(leftNode);
+      leftNode.setNext(newNode);
+      rightNode.setPrev(newNode);
+      ++length;
+    } else throw new IndexOutOfBoundsException("Index not available.");
+  }
+
+  /**
+   * Add element at first node. Set the newly created node as root node.
+   *
+   * @param data Add data node at beginning.
+   */
+  @SuppressWarnings({"PMD.LawOfDemeter", "nullness:argument.type.incompatible"})
+  @Override
+  public void addAtFirst(T data) {
+    Objects.requireNonNull(data, DATA_NON_NULL);
+    INode<T> newNode = new DoubleNode<>(data);
+    if (this.head == null) {
+      this.head = newNode;
+      this.tail = newNode;
+      newNode.setNext(tail);
+      newNode.setPrev(head);
+    } else {
+      tail.setNext(newNode);
+      newNode.setPrev(tail);
+      newNode.setNext(this.head);
+      head.setPrev(newNode);
+      this.head = newNode;
+    }
+    ++length;
+  }
+
+  @SuppressWarnings({"nullness:return.type.incompatible", "nullness:argument.type.incompatible"})
+  @Override
+  public INode<T> find(T data) {
+    Objects.requireNonNull(data, DATA_NON_NULL);
+    INode<T> node = new DoubleNode<>(data);
+    if (head.equals(node)) return head;
+    INode<T> startNode = head.getNext();
+    while (!head.isSame(startNode)) {
+      if (startNode.equals(node)) return startNode;
+      startNode = next(startNode);
+    }
+    return null;
+  }
+
+  @SuppressWarnings({
+    "nullness:assignment.type.incompatible",
+    "PMD.NullAssignment",
+    "nullness:argument.type.incompatible"
+  })
+  @Override
+  public boolean delete(T data) {
+    Objects.requireNonNull(data, DATA_NON_NULL);
+    if (head == null) return false;
+    INode<T> node = new DoubleNode<>(data);
+    if (head.equals(node)) {
+      if (head.isSame(head.getNext())) {
+        head = null;
+        tail = null;
+      } else {
+        head = head.getNext();
+        tail.setNext(head);
+        head.setPrev(tail);
+      }
+      --length;
+      return true;
+    }
+    INode<T> prevNode = head;
+    INode<T> currNode = head.getNext();
+    while (!head.isSame(currNode)) {
+      if (currNode.equals(node)) {
+        INode<T> nextNode = currNode.getNext();
+        prevNode.setNext(nextNode);
+        nextNode.setPrev(prevNode);
+        --length;
+        return true;
+      }
+      prevNode = currNode;
+      currNode = currNode.getNext();
+    }
+    return false;
+  }
+
+  @Override
+  public INode<T> get(int index) {
+    if (index < 0 || index > this.length - 1)
+      throw new IndexOutOfBoundsException("Index not available: " + index);
+    if (index == 0) return this.head;
+    if (index == this.length - 1) return tail;
+    int pointer = 0;
+    INode<T> pointerNode = this.head;
+    while (pointer != index) {
+      pointerNode = next(pointerNode);
+      ++pointer;
+    }
+    return pointerNode;
+  }
+
+  private INode<T> next(INode<T> node) {
+    return node.getNext();
+  }
+
+  @Override
+  public int size() {
+    return this.length;
+  }
+
+  public INode<T> getHead() {
+    return head;
+  }
+
+  public INode<T> getTail() {
+    return tail;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder(2);
+    sb.append('[');
+    INode<T> nextNode = this.head;
+    while (head != null) {
+      sb.append(nextNode);
+      nextNode = next(nextNode);
+      if (head.isSame(nextNode)) break;
+      sb.append(',');
+    }
+    sb.append(']');
+    return sb.toString();
+  }
+}
