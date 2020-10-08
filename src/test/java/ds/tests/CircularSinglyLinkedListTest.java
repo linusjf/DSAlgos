1e4120e23f6b5f29f9f17e2b7db9970d98defe1a
fernal73 <linus.fernandes@gmail.com>
2020-10-08 10:49:23 +0530

    Remove println statements

--- src/test/java/ds/tests/CircularSinglyLinkedListTest.java
+++ src/test/java/ds/tests/CircularSinglyLinkedListTest.java
@@ -404,9 +404,7 @@ class CircularSinglyLinkedListTest {
     void testSinglePrevious() {
       CircularSinglyLinkedList<Integer> list = new CircularSinglyLinkedList<>();
       list.add(TEN);
-      System.out.println(list);
       ListIterator<Integer> iter = list.getIterator();
-      System.out.println(iter);
       assertEquals(list.getHead().getData(), iter.previous(), VALUES_EQUAL);
       assertEquals(list.getHead().getData(), iter.next(), VALUES_EQUAL);
       assertEquals(list.getTail().getData(), iter.next(), VALUES_EQUAL);
