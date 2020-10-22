154d75a7b9b93ce6f442ff036375e5b1be0f77db
fernal73 <linus.fernandes@gmail.com>
2020-10-22 12:36:42 +0530

    refactor class and method

--- src/main/java/ds/knapsack/NaiveKnapsack.java
+++ src/main/java/ds/knapsack/NaiveKnapsack.java
@@ -7,38 +7,37 @@ import java.util.Map;
 
 public class NaiveKnapsack extends AbstractKnapsack<Solution<Integer>> {
 
-  final transient Map<WeightCountTuple, Integer> vals;
-
   public NaiveKnapsack(Item[] items, int capacity) {
     super(items, capacity);
-    this.vals = new HashMap<>();
   }
 
-  private void put(WeightCountTuple wct, Integer val) {
+  private void put(Map<WeightCountTuple, Integer> vals, WeightCountTuple wct, Integer val) {
     vals.put(wct, val);
   }
 
-  private boolean containsKey(WeightCountTuple wct) {
+  private boolean containsKey(Map<WeightCountTuple, Integer> vals, WeightCountTuple wct) {
     return vals.containsKey(wct);
   }
 
   @SuppressWarnings("all")
-  private Integer get(WeightCountTuple wct) {
+  private Integer get(Map<WeightCountTuple, Integer> vals, WeightCountTuple wct) {
     return vals.get(wct);
   }
 
   @Override
   public Solution<Integer> solve() {
     List<Item> itemsSolution = new ArrayList<>(0);
-    int maximalValue = knapsack(capacity, items.length, itemsSolution);
+    Map<WeightCountTuple, Integer> vals = new HashMap<>();
+    int maximalValue = knapsack(capacity, items.length, itemsSolution, vals);
     return new Solution<>(itemsSolution, maximalValue);
   }
 
-  private int knapsack(int totalWeight, int n, List<Item> solutionItems) {
+  private int knapsack(
+      int totalWeight, int n, List<Item> solutionItems, Map<WeightCountTuple, Integer> vals) {
     if (n == 0 || totalWeight == 0) return 0;
 
     WeightCountTuple wct = new WeightCountTuple(totalWeight, n - 1);
-    if (containsKey(wct)) return get(wct);
+    if (containsKey(vals, wct)) return get(vals, wct);
 
     /**
      * If weight of nth item is more than capacity W, then this item cannot be included in the
@@ -46,26 +45,26 @@ public class NaiveKnapsack extends AbstractKnapsack<Solution<Integer>> {
      */
     if (items[n - 1].weight > totalWeight) {
       solutionItems.remove(items[n - 1]);
-      int val = knapsack(totalWeight, n - 1, solutionItems);
-      put(wct, val);
+      int val = knapsack(totalWeight, n - 1, solutionItems, vals);
+      put(vals, wct, val);
       return val;
     }
 
     /** Return the maximum of two cases: (1) nth item included (2) not included * */
-    int notIncludedVal = knapsack(totalWeight, n - 1, solutionItems);
+    int notIncludedVal = knapsack(totalWeight, n - 1, solutionItems, vals);
     int newWeight = totalWeight - items[n - 1].weight;
-    int remainder = knapsack(newWeight, n - 1, solutionItems);
+    int remainder = knapsack(newWeight, n - 1, solutionItems, vals);
     int includedVal = items[n - 1].value + remainder;
     boolean included = includedVal > notIncludedVal;
     if (included) {
       if (!solutionItems.contains(items[n - 1])) {
         solutionItems.add(items[n - 1]);
       }
-      put(wct, includedVal);
+      put(vals, wct, includedVal);
       return includedVal;
     } else {
       solutionItems.remove(items[n - 1]);
-      put(wct, notIncludedVal);
+      put(vals, wct, notIncludedVal);
       return notIncludedVal;
     }
   }
