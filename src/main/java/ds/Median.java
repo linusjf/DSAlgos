6f286dc46e2b0db1f745bb50c1f0579cb036d202
fernal73 <linus.fernandes@gmail.com>
2021-01-19 06:21:19 +0530

    Add debugging messages

--- src/main/java/ds/Median.java
+++ src/main/java/ds/Median.java
@@ -2,25 +2,37 @@ package ds;
 
 import static ds.ArrayUtils.swap;
 
+import java.util.Random;
+
 public class Median {
+
+  private final Random random;
 
   private final long[] array;
 
   @SuppressWarnings("PMD.ArrayIsStoredDirectly")
   public Median(long... arr) {
     this.array = arr;
+    this.random = new Random();
   }
 
   public double find() {
     if (array.length == 0) return Double.NaN;
     if (array.length == 1) return array[0];
-    long pivot = array[array.length - 1];
+
+    // long pivot = array[array.length - 1];
+    // use random pivot
+    int pivotIndex = random.nextInt(array.length);
+    long pivot = array[pivotIndex];
     if (array.length % 2 == 1) {
       int medianIndex = partitionIt(0, array.length - 1, pivot, array.length >> 1);
+      System.out.println("medianIndex = " + medianIndex);
       return array[medianIndex];
     } else {
       int medIndex1 = partitionIt(0, array.length - 1, pivot, (array.length - 1) >> 1);
       int medIndex2 = partitionIt(0, array.length - 1, pivot, array.length >> 1);
+      System.out.println("medIndex1 = " + medIndex1);
+      System.out.println("medIndex2 = " + medIndex2);
       return 0.5 * (array[medIndex1] + array[medIndex2]);
     }
   }
@@ -36,8 +48,16 @@ public class Median {
       if (leftPtr >= rightPtr) break;
       else swap(array, leftPtr, rightPtr);
     }
+
     if (leftPtr == k) return leftPtr;
-    else if (leftPtr < k) return partitionIt(leftPtr + 1, right, array[right], k);
-    else return partitionIt(0, leftPtr - 1, array[leftPtr - 1], k);
+    if (leftPtr < k) {
+      int pivotIndex = random.nextInt(right - leftPtr);
+      long newPivot = array[leftPtr + 1 + pivotIndex];
+      return partitionIt(leftPtr + 1, right, newPivot, k);
+    } else {
+      int pivotIndex = random.nextInt(leftPtr);
+      long newPivot = array[pivotIndex];
+      return partitionIt(0, leftPtr - 1, newPivot, k);
+    }
   }
 }
--- src/test/java/ds/tests/MedianTest.java
+++ src/test/java/ds/tests/MedianTest.java
@@ -20,6 +20,7 @@ class MedianTest {
   @Test
   @DisplayName("MedianTest.testEmptyArray")
   void testEmptyArray() {
+    System.out.println("testEmptyArray");
     Median median = new Median(new long[0]);
     assertEquals(Double.NaN, median.find(), "NaN expected.");
   }
@@ -27,6 +28,7 @@ class MedianTest {
   @Test
   @DisplayName("MedianTest.testSingleElementArray")
   void testSingleElementArray() {
+    System.out.println("testSingleElementArray");
     long[] arr = {23};
     Median median = new Median(arr);
     assertEquals(23, median.find(), "23 expected.");
@@ -35,6 +37,7 @@ class MedianTest {
   @Test
   @DisplayName("MedianTest.testTwoElementArray")
   void testTwoElementArray() {
+    System.out.println("testTwoElementArray");
     long[] arr = {23, 24};
     Median median = new Median(arr);
     assertEquals(23.5, median.find(), "23.5 expected.");
@@ -43,6 +46,7 @@ class MedianTest {
   @Test
   @DisplayName("MedianTest.testThreeElementArray")
   void testThreeElementArray() {
+    System.out.println("testThreeElementArray");
     long[] arr = {23, 21, 24};
     Median median = new Median(arr);
     assertEquals(23, median.find(), "23 expected.");
@@ -51,22 +55,28 @@ class MedianTest {
   @Test
   @DisplayName("MedianTest.testMedianEven")
   void testMedianEven() {
+    System.out.println("testMedianEven");
     long[] arr = {23, 21, 20, 18, 17, 16, 0, 10, 9, 10};
-    Median median = new Median(arr);
-    assertEquals(16.5f, median.find(), "16.5 expected.");
+    Median median = new Median(arr.clone());
+    Arrays.sort(arr);
+    double expected = (arr[4] + arr[5]) / 2.0f;
+    assertEquals(expected, median.find(), () -> expected + " expected.");
   }
 
   @Test
   @DisplayName("MedianTest.testMedianOdd")
   void testMedianOdd() {
+    System.out.println("testMedianOdd");
     long[] arr = {23, 21, 20, 18, 18, 17, 16, 0, 10, 9, 10};
-    Median median = new Median(arr);
-    assertEquals(17.0f, median.find(), "17 expected.");
+    Median median = new Median(arr.clone());
+    Arrays.sort(arr);
+    assertEquals(arr[5], median.find(), () -> arr[5] + " expected.");
   }
 
   @Test
   @DisplayName("MedianTest.testRandomEven")
   void testRandomEven() {
+    System.out.println("testRandomEven");
     long[] arr = new long[10_000];
     Random random = new Random();
     for (int i = 0; i < 10_000; i++) arr[i] = random.nextInt();
@@ -79,6 +89,7 @@ class MedianTest {
   @Test
   @DisplayName("MedianTest.testRandomOdd")
   void testRandomOdd() {
+    System.out.println("testRandomOdd");
     long[] arr = new long[10_001];
     Random random = new Random();
     for (int i = 0; i < 10_001; i++) arr[i] = random.nextInt();
